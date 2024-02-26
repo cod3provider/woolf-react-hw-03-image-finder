b0223201f54e0104d7f9f4ab72443917e46c773b
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-26 11:53:06 +0100

    add styles to GalleryItem component

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,7 +1,10 @@
+import s from './ImageGalleryItem.module.css';
+
 const ImageGalleryItem = ({ largeImageURL, tags, webUrl }) => {
   return (
-    <li>
+    <li className={s.item}>
       <img
+        className={s.image}
         src={webUrl}
         alt={tags}
       />
