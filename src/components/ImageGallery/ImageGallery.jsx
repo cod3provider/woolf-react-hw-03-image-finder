a87cf168323243e33d28f25779c01e762c97b7f1
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-26 19:30:22 +0100

    add opening to modal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,15 +2,17 @@ import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
 import s from './ImageGallery.module.css';
 
-const ImageGallery = ({ images }) => {
+const ImageGallery = ({ images, onOpenModal }) => {
   return (
     <ul className={s.list}>
       {images.map(({ id, largeImageURL, tags, webformatURL }, idx) => (
         <ImageGalleryItem
+          openModalClick={onOpenModal}
           key={`${idx}+${id}`} // this resolving problems of backend when two children encountered with the same key
           largeUrl={largeImageURL}
           tags={tags}
           webUrl={webformatURL}
+
         />
       ))
       }
--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,12 +1,14 @@
 import s from './ImageGalleryItem.module.css';
+import { useState } from 'react';
 
-const ImageGalleryItem = ({ largeImageURL, tags, webUrl }) => {
+const ImageGalleryItem = ({ largeUrl, tags, webUrl, openModalClick }) => {
   return (
     <li className={s.item}>
       <img
         className={s.image}
         src={webUrl}
         alt={tags}
+        onClick={() => openModalClick(largeUrl, tags)}
       />
     </li>
   );
